25627b6673c4786b634a105c92fc4cb77bada1be
meaaditiya <aaditiyatyagi123@gmail.com>
2025-05-06 22:34:42 +0530

    patching

--- backend/app.js
+++ backend/app.js
@@ -33,7 +33,7 @@ app.use(cors({
     return callback(null, true);
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS','PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
