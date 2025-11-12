ab57e46b4dd0aab1ef6ffab04767c78f647cc180
Aaditiya Tyagi <aaditiyatyagi123@gmail.com>
2025-11-12 12:34:20 +0530

    unecessary caching removal

--- backend/app.js
+++ backend/app.js
@@ -33,23 +33,24 @@ app.use(corsMiddleware);
 const PORT = process.env.PORT || 5000;
 connectDB();
 
-app.use(cacheMiddleware);
+
 app.use("/api/admin", adminRoutes);
-app.use(queryRoutes);
-app.use(superAdminRoutes);
-app.use(announcementRoutes);
 app.use(BlogSubmission);
 app.use(BlogRoutes);
+app.use(queryRoutes);
+app.use(superAdminRoutes);
 app.use(ContactRoutes);
-app.use(ImageRoutes);
-app.use(SocialMediaEmbed);
+app.use(announcementRoutes);
 app.use(projectRoutes);
 app.use(EmailRoutes);
-app.use(ProfileRoutes);
-app.use(QuoteRoutes);
-app.use(CommunityPostRoutes);
 app.use(AudioMessageRoutes);
 app.use(StreamRoutes);
+app.use(ProfileRoutes);
+app.use(QuoteRoutes);
+app.use(cacheMiddleware);
+app.use(ImageRoutes);
+app.use(SocialMediaEmbed);
+app.use(CommunityPostRoutes);
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.listen(PORT, () => {
