516661540bf5c829f3a07da8b8722ec35c049c5f
Aaditiya Tyagi <aaditiyatyagi123@gmail.com>
2025-11-21 20:05:09 +0530

    final cors fix

--- backend/middlewares/corsMiddleware.js
+++ backend/middlewares/corsMiddleware.js
@@ -1,3 +1,4 @@
+// corsMiddleware.js
 const cors = require("cors");
 
 const allowedOrigins = [
@@ -14,27 +15,14 @@ const allowedOrigins = [
 const corsMiddleware = cors({
   origin: function(origin, callback) {
     if (!origin) return callback(null, true);
-    
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, origin);
+    if (!allowedOrigins.includes(origin)) {
+      return callback(new Error('CORS: Origin not allowed.'), false);
     }
-    
-    return callback(new Error('CORS: Origin not allowed.'), false);
+    return callback(null, true);
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Fingerprint-Data'],
-  exposedHeaders: ['Content-Length', 'X-Request-Id'],
-  maxAge: 3600
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS','PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 });
-const varyMiddleware = (req, res, next) => {
-  res.setHeader('Vary', 'Origin');
-  next();
-};
-const combinedCorsMiddleware = (req, res, next) => {
-  varyMiddleware(req, res, () => {
-    corsMiddleware(req, res, next);
-  });
-};
 
-module.exports = combinedCorsMiddleware;
+module.exports = corsMiddleware;
