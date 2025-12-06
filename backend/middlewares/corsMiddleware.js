a5dd8baca3f5b831a3af99017b4901715a30c5cf
Aaditiya Tyagi <157193513+meaaditiya@users.noreply.github.com>
2025-12-06 21:39:43 +0530

    Restrict CORS to allowed origins

--- backend/middlewares/corsMiddleware.js
+++ backend/middlewares/corsMiddleware.js
@@ -1,11 +1,25 @@
 const cors = require("cors");
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:5174',
+  'https://connectwithaaditiya.onrender.com',
+  'https://connectwithaaditiyamg.onrender.com',
+  'https://connectwithaaditiyaadmin.onrender.com',
+  'http://192.168.1.33:5174',
+  'http://192.168.1.33:5173',
+  'https://aaditiyatyagi.vercel.app'
+];
 
 const corsMiddleware = cors({
-  origin: (origin, callback) => {
-    return callback(null, true); // Allow all origins
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);
+    if (!allowedOrigins.includes(origin)) {
+      return callback(new Error('CORS: Origin not allowed.'), false);
+    }
+    return callback(null, true);
   },
-  credentials: true, // Allow cookies, tokens, authentication
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS','PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
 });
 
